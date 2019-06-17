605875e0eacb1328b4df2486b26228c0ce17a1d1
d0ni <xxxd0nixxx1993@gmail.com>
2019-06-17 11:49:03 +0300

    v2

--- client/router.jsx
+++ client/router.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { mount } from "react-mounter";
+import { FlowRouter } from "meteor/kadira:flow-router";
 
 import { MainLayuot } from "./layouts/MainLayout";
 import App from "./app";
